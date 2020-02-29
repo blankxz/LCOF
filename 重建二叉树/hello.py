b7d4f561598cb20f42a347b1b1e1220fbde8e99e
blankxz <993010776@qq.com>
2020-02-29 21:21:28 +0800

    重建二叉树

--- 重建二叉树/hello.py
+++ 重建二叉树/hello.py
@@ -1,9 +1,31 @@
-class Solution:
-    def replaceSpace(self, s) -> str:
-        return s.replace(" ",'%20')
-    def replaceSpace2(self, s) -> str:
-        return "".join(["%20" if c == " " else c for c in s])
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
 
-    
+class Solution:
+    def buildTree(self, preorder, inorder) -> TreeNode:
+        
+        def build(preorder,inorder):
+            if len(preorder)>0:
+                root = TreeNode(preorder[0])
+                ind = inorder.index(preorder[0])
+                root.left = build(preorder[1:1+ind],inorder[:ind])
+                root.right = build(preorder[1+ind:],inorder[ind+1:])
+                return root
+        return build(preorder,inorder)
 s = Solution()
-print(s.replaceSpace2("We are happy."))
+a = s.buildTree([3,9,20,15,7],[9,3,15,20,7])
+
+def pri(a):
+    if a == None:
+        return
+    if a != None:
+        print(a.val)
+    pri(a.left)
+    pri(a.right)
+    
+    
+pri(a)
