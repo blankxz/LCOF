ce65fb4a102fd6b50c33353a7e6d66017e80f658
blankxz <993010776@qq.com>
2020-03-04 00:17:33 +0800

    机器人的运动范围DFS“

--- 机器人的运动范围/hello.py
+++ 机器人的运动范围/hello.py
@@ -1,21 +1,27 @@
 class Solution:
     def movingCount(self, m: int, n: int, k: int) -> int:
-        dfs(38,39,1)
+        mat = [None] * m
+        for i in range(m):
+            mat[i] = [1]*n
+        mm = m
+        nn = n
         def dfs(i,j,k):
             s = 0
             a = i
             b = j
-            while a:
+            while a>0:
                 s += a%10
-                a/=10
-            while b:
+                a//=10
+            while b>0:
                 s += b%10
-                b/=10
-            print(s)
-                    
-            # if not 0<=i<m or not 0<=j<n or 
+                b//=10
+            if not 0<=i<mm or not 0<=j<nn or s > k or mat[i][j] == 0: return 0
+            mat[i][j] = 0
+            return 1 + dfs(i+1,j,k) + dfs(i,j+1,k) + dfs(i-1,j,k) + dfs(i,j-1,k)
+            
+        return dfs(0,0,k)
+            
         
 s = Solution()
 a = s.movingCount(2,3,1)
-
 print(a)
