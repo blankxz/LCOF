bd11fb4f940840bff26c092a7ec459bad99c9ed3
blankxz <993010776@qq.com>
2020-03-09 23:49:33 +0800

    表示数值的字符串

--- testFile.py
+++ testFile.py
@@ -8,3 +8,23 @@ print(a)
 a = [1,2,5,4,8,6,5]
 a = sorted(a)
 print(a)
+
+
+a = 1
+print(id(a))
+def fun(a):
+    print(id(a))
+    a = 2
+    
+fun(a)
+print(a)
+
+a = []
+print(id(a))
+def fun(a):
+    print(id(a))
+    a.append(1)
+fun(a)
+print(a)
+
+# Python的变量就是在特定的时间引用了一个特定的对象。
