9c3f82be2a83b580e4209c0c7248a93f301a983b
blankxz <993010776@qq.com>
2020-03-10 23:48:56 +0800

    调整数组顺序使奇数位于偶数前面

--- testFile.py
+++ testFile.py
@@ -28,3 +28,67 @@ fun(a)
 print(a)
 
 # Python的变量就是在特定的时间引用了一个特定的对象。
+
+def print_1():
+    a = '11223'
+    def print_2():
+        print(a)
+    return print_2
+print_1()()
+
+import functools
+
+
+def log(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        print('call %s():' % func.__name__)
+        print('args = {}'.format(*args))
+        return func(*args, **kwargs)
+
+    return wrapper
+
+# @log
+def test(p):
+    print(test.__name__ + " param: " + p)
+    
+# test("I'm a param")
+
+w = log(test)
+w('111')
+
+
+def log(level):
+    def info(func):
+        
+        def say(*args, **kwargs):
+            print("[DEBUG]: enter {}()".format(func.__name__))
+            print(level)
+            return func(*args, **kwargs)
+        return say
+    return info
+
+
+@log('1')
+def hello(a):
+    print(a)
+    
+@log('2')
+def by():
+    print('by')
+    
+hello('aaa')
+by()
+
+
+def dict(**kwargs):
+    print(kwargs)
+    return kwargs
+ 
+mydict = dict(system="系统", China="中国", link="联接")
+x = 'system'
+if x in mydict:
+    print(x)
+    print("中文意思：", mydict[x])
+else:
+    print("抱歉，没找到。")
