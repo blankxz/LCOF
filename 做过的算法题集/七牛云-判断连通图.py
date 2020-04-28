2857b0ef5a72b5b6e905df418f6a81d7e29c7cd2
blankxz <993010776@qq.com>
2020-04-28 20:41:49 +0800

    new

---
+++ 做过的算法题集/七牛云-判断连通图.py
@@ -0,0 +1,22 @@
+n, m = list(map(int,input().split()))
+dataMap = [[0]*n for i in range(n)]
+for i in range(m):
+    a = list(map(int,input().split()))
+    dataMap[a[0] - 1][a[1] - 1] = 1
+    dataMap[a[1] - 1][a[0] - 1] = 1
+count = 0
+visited = [0]*n
+def dfs(i):
+    visited[i] = 1
+    global count
+    count += 1
+    if count == n:
+        return
+    for j in range(n):
+        if dataMap[i][j] == 1 and not visited[j]:
+            dfs(j)
+dfs(0)
+if count!=n:
+    print('NO')
+else:
+    print('YES')
---
+++ 做过的算法题集/七牛云-消消乐.py
@@ -0,0 +1,40 @@
+t = int(input())
+for i in range(t):
+    s = list(input())
+    if len(s) == 1:
+        print('No')
+        continue
+    while 1:
+        n = 0
+        m = 0
+        for j in range(len(s)):
+            if len(s[n:m]) == 2:
+                for x in range(n, m):
+                    s[x] = '0'
+                n = j
+                m = n + 1
+                continue
+            if s[j]!=s[n]:
+                n = j
+                m = n+1
+            else:
+                m += 1
+            # print(s[n:m])
+        if len(s[n:m]) == 2:
+            for x in range(n, m):
+                s[x] = '0'
+        r = []
+        # print(s)
+        for x in s:
+            if x != '0':
+                r.append(x)
+        # print(r)
+        # print(s)
+        if r == s:
+            break
+        else:
+            s = r
+    if not s:
+        print('Yes')
+    else:
+        print('No')
